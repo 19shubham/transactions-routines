1cf2c4d4b771c0bd804d772208b3aa1a8f2b3a1c
shubhamjaroli <shubham.jaroli@leadsquared.com>
2024-11-09 11:11:52 +0530

    added operation type enums and its functions

--- models/OperationsType.go
+++ models/OperationsType.go
@@ -1,1 +1,35 @@
 package models
+
+import (
+	"errors"
+)
+
+type OperationType int
+
+const (
+	NormalPurchase      OperationType = 1
+	InstallmentPurchase OperationType = 2
+	Withdrawal          OperationType = 3
+	CreditVoucher       OperationType = 4
+)
+
+// Int - method will convert and return int value of enum operation type.
+func (ot OperationType) Int() int {
+	return int(ot)
+}
+
+// GetDescription - method will return description from int val of operation type and error if none of the type matches.
+func (ot OperationType) GetDescription() (string, error) {
+	switch ot {
+	case 1:
+		return "Normal Purchase", nil
+	case 2:
+		return "Purchase with installments", nil
+	case 3:
+		return "Withdrawal", nil
+	case 4:
+		return "Credit Voucher", nil
+	default:
+		return "", errors.New("invalid operation type")
+	}
+}
